b9ebfa39451c2ca27ff47252d71df51840c55e01
raynardj <raynard@rasenn.com>
2019-02-01 16:38:31 +0800

    init files for modules

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name="forge",
+    name="ai-forge",
     version="0.1.0",
     author="raynardj",
     author_email="raynard@rasenn.com",
     description="Ray's tool for managing machine learning",
+    packages = find_packages(),
     py_modules=['forgebox','forge',],
 )
