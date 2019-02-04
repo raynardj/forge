e469a592348ff5d7648c5fc2de3b2ce3d93320c9
raynardj <raynard@rasenn.com>
2019-02-04 11:42:39 +0800

    improve fa icon

--- forge/app/views/__init__.py
+++ forge/app/views/__init__.py
@@ -27,14 +27,14 @@ def page_not_found(e):
 from .post_view import PostView
 from .ml_view import taskView,formatView,hyperParamView,weightView,hyperParamWeightView
 
-web_cate = {"category":"Web", "category_icon":"dashboard"}
+web_cate = {"category":"Web", "category_icon":"fa-dashboard"}
 appbuilder.add_view(PostView, "posts", icon="fa-book", **web_cate )
 
-ml_cate = {"category":"ML", "category_icon":"dashboard"}
-appbuilder.add_view(taskView, "Tasks", icon="fa-book", **ml_cate )
-appbuilder.add_view(formatView, "Config Formats", icon="fa-book", **ml_cate )
-appbuilder.add_view(hyperParamView, "Hyper Parameters", icon="fa-book", **ml_cate )
-appbuilder.add_view(weightView, "Model Weights", icon="fa-book", **ml_cate )
+ml_cate = {"category":"ML", "category_icon":"fa-magic"}
+appbuilder.add_view(taskView, "Tasks", icon="fa-tasks", **ml_cate )
+appbuilder.add_view(formatView, "Config Formats", icon="fa-star-half-o", **ml_cate )
+appbuilder.add_view(hyperParamView, "Hyper Parameters", icon="fa-flask", **ml_cate )
+appbuilder.add_view(weightView, "Model Weights", icon="fa-cubes", **ml_cate )
 
 db.create_all()
 
