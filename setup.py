62ad2c76d7208f5389c40439be947e24a8a9a455
Raynard Jon (Zhang,Xiaochen) <raynard@rasenn.com>
2019-02-14 15:48:12 +0800

    Update setup.py
    fix setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email="raynard@rasenn.com",
     description="Ray's tool for managing machine learning",
     packages = find_packages(),
-    py_modules=['forgebox','./forge/*'],
+    py_modules=['forgebox','./forge/*','./forge/app/*',],
     install_requires = [
         "flask==0.12.4",
         "flask_appbuilder==1.10.0",
