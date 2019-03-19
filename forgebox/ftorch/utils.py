87724895f461b87ca477e3e11f40e307b4eca3b1
raynardj <raynard@rasenn.com>
2019-03-19 19:00:26 +0800

    save model and load model

--- forgebox/ftorch/utils.py
+++ forgebox/ftorch/utils.py
@@ -1,5 +1,6 @@
 from .train import JUPYTER
 if JUPYTER: from IPython.display import display
+import torch
 
 def mars(printstuff):
     """
@@ -8,3 +9,9 @@ def mars(printstuff):
     """
     if JUPYTER:display(printstuff)
     else:print(printstuff)
+
+def save_model(model,path):
+    torch.save(model.state_dict(), path)
+
+def load_model(model,path):
+    model.load_state_dict(torch.load(path))
