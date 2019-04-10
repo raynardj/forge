ff93617cdf0421b497ac3f4c809cf61c67f52e3b
raynardj <raynard@rasenn.com>
2019-04-10 14:11:21 +0800

    gelu activation

--- forgebox/ftorch/layers.py
+++ forgebox/ftorch/layers.py
@@ -230,3 +230,12 @@ class MultiMaskLSTM(nn.Module):
         outputs = tuple(output.bmm(mask).squeeze(-1) for mask in masks)
 
         return outputs, masks
+
+class GELU(nn.Module):
+    """
+    Paper Section 3.4, last paragraph notice that BERT used the GELU instead of RELU
+    <iframe src="https://www.desmos.com/calculator/fgpckn1i1m?embed" width="500px" height="500px" style="border: 1px solid #ccc" frameborder=0></iframe>
+    """
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
